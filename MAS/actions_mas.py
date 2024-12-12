dc4503f88fd54a0a68ff630080f84d6867768a4f
Rocco Paolillo <36846038+RoccoPaolillo@users.noreply.github.com>
2024-12-12 15:19:15 +0100

    MAS FOLDER

--- MAS/actions_mas.py
+++ MAS/actions_mas.py
@@ -3,17 +3,6 @@ import random
 import turtle
 import threading
 import queue
-import networkx as nx
-#import matplotlib
-#matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-import numpy
-import random
-import os
-import csv
-import pandas as pd
-import re
-import math
 
 # Coda per inviare richieste di query
 query_queue = queue.Queue()
@@ -29,7 +18,6 @@ from phidias.Types import *
 
 import configparser
 from owlready2 import *
-from front_end_mas import *
 
 config = configparser.ConfigParser()
 config.read('config_mas.ini')
@@ -75,14 +63,8 @@ STEP_BREAKDOWN = 50
 STEP_DURATIN = 0.005
 
 # Worker-Turtle dictionary
-# dict_turtle = {}
-# G = nx.Graph()
-# agents = get_agents_names()[1:]
-# G.add_nodes_from(agents)
-# pos = nx.spring_layout(G, seed=numpy.random.seed(1229))
-# agents = get_agents_names()[1:]
-# G.add_nodes_from(agents)
-# pos = nx.spring_layout(G, seed=231)
+dict_turtle = {}
+
 
 # ---------------------------------------------------------------------
 # Ontology section
@@ -97,144 +79,7 @@ Rest_Time = 3
 # Timer tick
 TICK = 0.1
 
-# ---------------------------------------------------------------------
-# Agent section
-# ---------------------------------------------------------------------
 
-# Thread che esegue le query SPARQL
-def query_thread():
-    my_world = owlready2.World()
-    my_world.get_ontology(FILE_NAME).load()  # path to the owl file
-
-    while True:
-        q, result_event = query_queue.get()  # Ottieni la query e l'evento di sincronizzazione
-
-        if q is None:  # Esci dal ciclo quando ricevi None
-            break
-
-        graph = my_world.as_rdflib_graph()
-        result = list(graph.query(q))  # Esegui la query
-        result_queue.put(result)  # Inserisci il risultato nella coda dei risultati
-        result_event.set()  # Notifica che il risultato è pronto
-
-
-# Avvia il thread delle query
-query_thread_instance = threading.Thread(target=query_thread)
-query_thread_instance.start()
-
-
-# Funzione per ottenere i nomi degli agenti (inviando la query al thread dedicato)
-def get_agents_names():
-    agents = []
-    q = PREFIX + f" SELECT ?subj" + " WHERE { "
-    q = q + f"?subj rdf:type {ONTO_NAME}:Agent." + "}"
-    
-    result_event = threading.Event()  # Evento per sincronizzare il risultato
-    query_queue.put((q, result_event))  # Invia la query al thread dedicato
-
-    result_event.wait()  # Aspetta che il risultato sia pronto
-
-    result = result_queue.get()  # Ottieni il risultato dalla coda
-
-    for res in result:
-        subj = str(res).split(",")[0]
-        subj = subj.split("#")[1][:-2]
-        agents.append(subj)
-    
-#    agents = list(map(lambda x: re.sub(r'[0-9]', '',x), agents))
-
-    return agents
-
-def get_scholars_names():
-    scholars = []
-    q = PREFIX + f" SELECT ?subj" + " WHERE { "
-    q = q + f"?subj rdf:type {ONTO_NAME}:Scholar." + "}"
-
-    result_event = threading.Event()  # Evento per sincronizzare il risultato
-    query_queue.put((q, result_event))  # Invia la query al thread dedicato
-
-    result_event.wait()  # Aspetta che il risultato sia pronto
-
-    result = result_queue.get()  # Ottieni il risultato dalla coda
-
-    for res in result:
-        subj = str(res).split(",")[0]
-        subj = subj.split("#")[1][:-2]
-        scholars.append(subj)
-    
-#    scholars = list(map(lambda x: re.sub(r'[0-9]', '',x), scholars))
-
-    return scholars
-
-def get_universities_names():
-    universities = []
-    q = PREFIX + f" SELECT ?subj" + " WHERE { "
-    q = q + f"?subj rdf:type {ONTO_NAME}:University." + "}"
-
-    result_event = threading.Event()  # Evento per sincronizzare il risultato
-    query_queue.put((q, result_event))  # Invia la query al thread dedicato
-
-    result_event.wait()  # Aspetta che il risultato sia pronto
-
-    result = result_queue.get()  # Ottieni il risultato dalla coda
-
-    for res in result:
-        subj = str(res).split(",")[0]
-        subj = subj.split("#")[1][:-2]
-        universities.append(subj)
-    
-#    universities = list(map(lambda x: re.sub(r'[0-9]', '',x), universities))
-
-    return universities
-
-def get_fields_names():
-    fields = []
-    q = PREFIX + f" SELECT ?subj" + " WHERE { "
-    q = q + f"?subj rdf:type {ONTO_NAME}:Field." + "}"
-
-    result_event = threading.Event()  # Evento per sincronizzare il risultato
-    query_queue.put((q, result_event))  # Invia la query al thread dedicato
-
-    result_event.wait()  # Aspetta che il risultato sia pronto
-
-    result = result_queue.get()  # Ottieni il risultato dalla coda
-
-    for res in result:
-        subj = str(res).split(",")[0]
-        subj = subj.split("#")[1][:-2]
-        fields.append(subj)
-    
-#    fields = list(map(lambda x: re.sub(r'[0-9]', '',x), fields))
-
-    return fields
-
-def get_newcomers_names():
-    newcomers = []
-    q = PREFIX + f" SELECT ?subj" + " WHERE { "
-    q = q + f"?subj rdf:type {ONTO_NAME}:Newcomers." + "}"
-
-    result_event = threading.Event()  # Evento per sincronizzare il risultato
-    query_queue.put((q, result_event))  # Invia la query al thread dedicato
-
-    result_event.wait()  # Aspetta che il risultato sia pronto
-
-    result = result_queue.get()  # Ottieni il risultato dalla coda
-
-    for res in result:
-        subj = str(res).split(",")[0]
-        subj = subj.split("#")[1][:-2]
-        newcomers.append(subj)
-    
-#    newcomers = list(map(lambda x: re.sub(r'[0-9]', '',x), newcomers))
-
-    return newcomers
-
-agents = get_agents_names()[1:]
-scholars = get_scholars_names()[1:]
-universities = get_universities_names()[1:]
-newcomers = get_newcomers_names()[1:]
-fields = get_fields_names()[1:]
-newcomers = get_newcomers_names()[1:]
 
 try:
     my_onto = get_ontology(FILE_NAME).load()
@@ -329,7 +174,7 @@ class init(Procedure): pass
 # Import OWL triples
 class load(Procedure): pass
 # Turning triples to beliefs
-class pre_process(Procedure): pass
+class turn(Procedure): pass
 
 
 
@@ -452,7 +297,7 @@ class TaskDetect(Sensor):
            pos_x = random.randint(-N // 2, N // 2)
            pos_y = random.randint(-N // 2, N // 2)
            print(f"Generating task on position ({pos_x}, {pos_y})...")
-           self.assert_belief(TASK())
+           self.assert_belief(TASK(pos_x, pos_y))
 
 
 
@@ -487,6 +332,51 @@ class Timer(Sensor):
                 return
 
 
+# ---------------------------------------------------------------------
+# Agent section
+# ---------------------------------------------------------------------
+
+# Thread che esegue le query SPARQL
+def query_thread():
+    my_world = owlready2.World()
+    my_world.get_ontology(FILE_NAME).load()  # path to the owl file
+
+    while True:
+        q, result_event = query_queue.get()  # Ottieni la query e l'evento di sincronizzazione
+
+        if q is None:  # Esci dal ciclo quando ricevi None
+            break
+
+        graph = my_world.as_rdflib_graph()
+        result = list(graph.query(q))  # Esegui la query
+        result_queue.put(result)  # Inserisci il risultato nella coda dei risultati
+        result_event.set()  # Notifica che il risultato è pronto
+
+
+# Avvia il thread delle query
+query_thread_instance = threading.Thread(target=query_thread)
+query_thread_instance.start()
+
+
+# Funzione per ottenere i nomi degli agenti (inviando la query al thread dedicato)
+def get_agents_names():
+    agents = []
+    q = PREFIX + f" SELECT ?subj" + " WHERE { "
+    q = q + f"?subj rdf:type {ONTO_NAME}:Agent." + "}"
+
+    result_event = threading.Event()  # Evento per sincronizzare il risultato
+    query_queue.put((q, result_event))  # Invia la query al thread dedicato
+
+    result_event.wait()  # Aspetta che il risultato sia pronto
+
+    result = result_queue.get()  # Ottieni il risultato dalla coda
+
+    for res in result:
+        subj = str(res).split(",")[0]
+        subj = subj.split("#")[1][:-2]
+        agents.append(subj)
+
+    return agents
 
 
 # Funzione per terminare il thread in sicurezza
@@ -583,128 +473,25 @@ class move_turtle(Action):
 
 
 
-# def turtle_thread_func():
-#    wn = turtle.Screen()
-#    wn.title("Workers jobs assignment")
+def turtle_thread_func():
+    wn = turtle.Screen()
+    wn.title("Workers jobs assignment")
 
-#    agents = get_agents_names()[1:]
+    # Mantenere la finestra sempre in primo piano
+    cv = wn.cv
+    rootwindow = cv._rootwindow
+    rootwindow.attributes("-topmost", 1)  # Imposta la finestra sempre in primo piano
+    rootwindow.update()  # Aggiorna la finestra
 
-#    for i in range(len(agents)):
-#       dict_turtle[agents[i]] = turtle.Turtle()
+    agents = get_agents_names()[1:]
 
-#    wn.mainloop()
-   
-# class network_init(Action):
-#     def execute(self):
-#        dflink = pd.read_csv('links.csv', delimiter = ";") # dataframe links
-         # empty graph
-#         G.remove_edges_from(list(G.edges()))
-#         agents = get_agents_names()[1:]
-#         G.add_nodes_from(agents)
-#         pos = nx.spring_layout(G, seed=231)
-#        pos = nx.spring_layout(G, seed=numpy.random.seed(1229))
-#         vis_network()
-#        plt.clf() 
-#        nx.draw(G,with_labels=True)
-#        plt.show()
-        
-class new_affiliation(Action):
-    def execute(self,arg0,arg1):
-        node_1 = str(arg0).split("'")[3]
-        node_2 = str(arg1).split("'")[3]
-        G.add_edge(node_1, node_2, color = "orange", weight = 4, label = "affil", edgestyle = "solid") # str(arg3).split()[0])
-        vis_network()
-        
-class co_authorshiplink(Action):
-    def execute(self,arg0,arg1):
-        node_1 = str(arg0).split("'")[3]
-        node_2 = str(arg1).split("'")[3]
-        G.add_edge(node_1, node_2, color = "lightgrey", weight = 2, label = "coauthor", edgestyle = "dashed") # str(arg3).split()[0])
-        vis_network()
-        
-class affiliationlink(Action):
-    def execute(self,arg0,arg1):
-        node_1 = str(arg0).split("'")[3]
-        node_2 = str(arg1).split("'")[3]
-        G.add_edge(node_1, node_2, color = "lightgrey", weight = 2, label = "affil", edgestyle = "dotted") # str(arg3).split()[0])
-        vis_network()
-        
-class topauthorlink(Action):
-    def execute(self,arg0,arg1):
-        node_1 = str(arg0).split("'")[3]
-        node_2 = str(arg1).split("'")[3]
-        G.add_edge(node_1, node_2, color = "lightgrey", weight = 2, label = "topauthor", edgestyle = "dashdot") # str(arg3).split()[0])
-        vis_network()
-        
-class selectforlink(Action):
-    def execute(self,arg0,arg1):
-        node_1 = str(arg0).split("'")[3]
-        node_2 = str(arg1).split("'")[3]
-        G.add_edge(node_1, node_2, color = "darkgrey", weight = 4, label  = "selected", edgestyle = "dashed") # str(arg3).split()[0])
-        vis_network()
-        
-G = nx.Graph()
+    for i in range(len(agents)):
+        dict_turtle[agents[i]] = turtle.Turtle()
 
-# G.add_nodes_from(agents)
-G.add_nodes_from(universities)
-G.add_nodes_from(fields)
-G.add_nodes_from(agents)
-G.add_nodes_from(newcomers)
-
-# color_map = ['red' if node in universities else 'green' if node in fields else 'orange' if node in newcomers else "cyan" for node in G] 
-color_map = ['orange' if node in newcomers else 'white' for node in G] 
+    wn.mainloop()
 
 
-#colors_edges = nx.get_edge_attributes(G,"color").values()
-# color_map = ['red' if node in universities elif 'green' if node in fields else "blue" for node in G] 
-
-# color_map = []
-
-# for node in G:
-#     if node in universities:
-#         node.color_map = "red"
-#     elif node in fields:
-#         node.color_map = "green"
-#     else:
-#         node.color_map = "blue"
-        
-        
-        
-        
-#             else 'violet' for node in G]  G.nodes[node]["color"]
-
-#df = pd.read_csv('nodes.csv', delimiter = ";")  # dataframe nodes
-#G.from_pandas_dataframe(df)
-# for node in G.nodes():  # to add attribute color to nodes extracting from the dataframe
-#    G.nodes[node]["color"] = df[(df.node == node)]["color"].item()
-
-# features for the layout of the graph
-
-# colors = [node[1]['color'] for node in G.nodes(data=True)] # color of nodes
-
-pos = nx.spring_layout(G , seed=numpy.random.seed(5581), scale = 8) # k = 300, iterations = 70)  # 15495 #5581 # 1933
-# G = nx.relabel_nodes(G, lambda x: ''.join([i for i in x if not i.isdigit()]))
-
-def vis_network():
-#        wm = plt.get_current_fig_manager() 
-#        wm.window.attributes('-topmost', 1)
-#        wm.window.attributes('-topmost', 0)
-#        plt.gcf().canvas.get_tk_widget().focus_force() 
-        colors_edges = nx.get_edge_attributes(G,"color").values()
-        edges = G.edges()
-        weights_edges = [G[u][v]['weight'] for u,v in edges]# nx.get_edge_attributes(G,'weight').values()        
-        plt.clf()
-#        fig = plt.figure()
-        nx.draw(G,with_labels=True, node_color=color_map , edge_color = colors_edges, width = weights_edges ,  pos = pos,
-                font_size = 18)
-        nx.draw_networkx_edge_labels(G, edge_labels=nx.get_edge_attributes(G,'label'), 
-                                     label_pos= 0.7,  pos = pos,
-                                     font_size = 14) # edge_color = colors_edges,  pos = pos)5
-#        fig.canvas.manager.window.attributes('-topmost', 1)
-        plt.show()
-
-
-# Avviare il thread del network
-ntw_thread = threading.Thread(target=vis_network)
-ntw_thread.daemon = True
-ntw_thread.start()
+# Avviare il thread della tartaruga
+turtle_thread = threading.Thread(target=turtle_thread_func)
+turtle_thread.daemon = True
+turtle_thread.start()
--- MAS/front_end_mas.py
+++ MAS/front_end_mas.py
@@ -5,55 +5,35 @@ from actions_mas import *
 # PHIDIAS rules variable declaration
 # ---------------------------------------------------------------------
 
-def_vars("X", "Y", "D", "H", "Z", "L", "M", "A", "D", "W","S","U")
+def_vars("X", "Y", "D", "H", "Z", "L", "M", "A", "D", "W")
 
 # ---------------------------------------------------------------------
 # Agents section
 # ---------------------------------------------------------------------
 
 agents = get_agents_names()[1:]
-scholars = get_scholars_names()[1:]
-universities = get_universities_names()[1:]
-newcomers = get_newcomers_names()[1:]
 
 if len(agents)==0:
-   print("\nWARNING: Agents list is empty. Please initialize the ontology with init() from the eShell then restart.")
+   print("\nWARNING: Agents list is empty. Please initialize the ontology with init() from the eShell, then restart Semas.")
 else:
    print("Agents list: ", agents)
-
 
 
 def create_agents(class_name):
     def main(self):
         # MoveAndCompleteJob intention
-#         +TASK(X, Y, A)[{'from': M}] >> [show_line("\n",A," is moving to (", X, ",", Y, "), received task from ", M), move_turtle(A, X, Y), +COMM("DONE")[{'to': 'main'}]]
-         +TASK(X)[{'from': M}] >> [pubby(X), +COMM("DONE")[{'to': 'main'}]]
+        +TASK(X, Y, A)[{'from': M}] >> [show_line("\n",A," is moving to (", X, ",", Y, "), received task from ", M), move_turtle(A, X, Y), +COMM("DONE")[{'to': 'main'}]]
 
-
-
-#        +NETNEW(X, Y)[{'from': M}] >> [show_line(create_link(X,Y))[{'to': 'main'}]]
-    # Creiamo una nuova classe con il metodo 'main' definito sopra
     return type(class_name, (Agent,), {"main": main})
 
 for i in range(len(agents)):
-    # class_name = f"{ID_PREFIX}{i+1}"
     globals()[agents[i]] = create_agents(agents[i])
 
-# Ora puoi creare istanze delle nuove classi e chiamare il loro metodo main
 for i in range(len(agents)):
-    # class_name = f"{ID_PREFIX}{i+1}"
     instance = globals()[agents[i]]()
     instance.main()
-    
-for i in range(len(scholars)):
-    # class_name = f"{ID_PREFIX}{i+1}"
-    globals()[scholars[i]] = create_agents(scholars[i])
 
-# Ora puoi creare istanze delle nuove classi e chiamare il loro metodo main
-for i in range(len(scholars)):
-    # class_name = f"{ID_PREFIX}{i+1}"
-    instance = globals()[scholars[i]]()
-    instance.main()
+
 
 # ---------------------------------------------------------------------
 # Agent 'main'
@@ -66,20 +46,12 @@ class main(Agent):
         init() >> [show_line("\nInitialiting Ontology...\n"), initWorld(), declareRules(), saveOnto()]
 
         # Importing related triples
-        load() >> [show_line("\nAsserting all OWL 2 triples beliefs...\n"), assert_beliefs_triples(), show_line("\nTurning triples beliefs into Semas beliefs...\n"), pre_process()]
-#        pre_process() / TRIPLE(X, "hasLedger",Z) >> [-TRIPLE(X,"hasLedger",Z), +LEDGER(X,"0"), AssignId(X), pre_process()]
-
-# FOSSR DAYS        
-        pre_process() / TRIPLE(X, "coAuthorWith", Y) >> [-TRIPLE(X, "coAuthorWith", Y), +CoAuthorship(X, Y), co_authorshiplink(X,Y), pre_process()]
-        pre_process() / TRIPLE(X, "hasAffiliationWith", Y) >> [-TRIPLE(X, "hasAffiliationWith", Y), +Affiliation(X, Y), affiliationlink(X,Y), pre_process()]
-        pre_process() / TRIPLE(X, "isTopAuthorIn", Y) >> [-TRIPLE(X, "isTopAuthorIn", Y), +TopAuthorship(X, Y), topauthorlink(X,Y), pre_process()]
-        pre_process() / TRIPLE(X, "selectedFor", Y) >> [-TRIPLE(X, "selectedFor", Y), +Selectionship(X, Y), selectforlink(X,Y) , pre_process()]
-# FOSSR DAYS
+        load() >> [show_line("\nAsserting all OWL 2 triples beliefs...\n"), assert_beliefs_triples(), show_line("\nTurning triples beliefs into Semas beliefs...\n"), turn()]
+        turn() / TRIPLE(X, "hasLedger",Z) >> [-TRIPLE(X,"hasLedger",Z), +LEDGER(X,"0"), AssignId(X), turn()]
 
         # desires
         setup() / WORKTIME(W) >> [show_line("Setup worktime again...\n"), load(), -WORKTIME(W), +WORKTIME(0)]
         setup() >> [show_line("Setup worktime...\n"), +WORKTIME(0), +MAX_WORK_TIME(Max_Work_Time), +MAX_WORKDAY_TIME(Max_WorkDay_Time), +REST_TIME(Rest_Time)]
-#        work() >> [show_line("Starting task detection...\n"), Timer(Max_Work_Time).start(), TaskDetect().start(), show_line("Workers on duty...")]
         work() >> [show_line("Starting task detection...\n"), Timer(Max_Work_Time).start(), TaskDetect().start(), show_line("Workers on duty...")]
 
         # AssignJob intentions
@@ -97,19 +69,21 @@ class main(Agent):
         noduty() / REST_TIME(X) >> [rest(X), work()]
 
         # Stop work intention
-        stopwork() / ((AGT(A, D) & DUTY(D))) >> [show_line("\n-------------------------> Stopping ", A), -DUTY(D), stopwork()]
+        stopwork() / (AGT(A, D) & DUTY(D)) >> [show_line("\n-------------------------> Stopping ", A), -DUTY(D), stopwork()]
         stopwork() >> [show_line("\nAll workers were stopped. Starting payment process."), pay()]
 
         # pay desires
         pay() / LEDGER(Z, H) >> [show_line("\nSending payment to ",Z, " for ",H," tasks..."), -LEDGER(Z, H), pay()]
         pay() >> [show_line("\nPayments completed.")]
-        DesireGoalFor(X) / (Selectionship(S,U) & TopAuthorship(Y, X) & Affiliation(Y, U)) >> [show_line("Direct match found at ",U,".\n"), -TopAuthorship(Y, X), +ProposeCoauthorship(Y, X), +AcceptOffer(S,X,U), DesireGoalFor(X)]
 
-        DesireGoalFor(X) / (Selectionship(S,U) & TopAuthorship(Y, X) & CoAuthorship(Z, Y)  & Affiliation(Z, U)) >> [show_line("Indirect match found at ",U,".\n"), -CoAuthorship(Z, Y), +coauthorIndirect(Z, U,Y,X), +AcceptOffer(S,X,U), DesireGoalFor(X)]
 
-        
-        +coauthorIndirect(Z, U,Y,X) >> [show_line(Z," at ", U, " is co-author with ",Y,", a top-author in the field of ",X,".\n")]
-        +ProposeCoauthorship(Y,X) >> [show_line("Propose co-authorship with ",Y," as top-author in the field of ",X,".\n")]
-        +AcceptOffer(S,X,U) >> [show_line(S," should accept offer from University ",U," with co-authors of top-authors in field of ",X,".\n"),-TRIPLE(S, "hasAffiliationWith", U), +Affiliation(S,U), new_affiliation(S,U), pre_process()]
+
+for i in range(len(agents)):
+    instance = globals()[agents[i]]()
+    instance.start()
 
 main().start()
+
+# PHIDIAS.achieve(load(), "main")
+# PHIDIAS.achieve(setup(), "main")
+# PHIDIAS.achieve(work(), "main")
